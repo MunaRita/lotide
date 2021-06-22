1774b1735f236ba4a8d1e6737182fb1a6e80ac6a
vagrant <chiomaanierobi@gmail.com>
2021-06-22 02:24:24 +0000

    lotide challenge implement head

---
+++ head.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const head = function(actual) {
+  return (actual[0]);
+};
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
