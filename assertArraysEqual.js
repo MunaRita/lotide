f790da2fc9bf8877622f585c38d2888979c4febe
vagrant <chiomaanierobi@gmail.com>
2021-06-26 21:44:32 +0000

    Implementing the map function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -19,14 +19,14 @@ const assertArraysEqual = function(actual,expected) {
   }
   }
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+//   }
 
-};
+// };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
