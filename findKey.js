9316e6705a04142289eb92b527e5d24580666cd7
vagrant <chiomaanierobi@gmail.com>
2021-06-27 00:27:32 +0000

    implement findkey

--- findKey.js
+++ findKey.js
@@ -1,4 +1,14 @@
 
+const findKey = function(object, callback) {
+  for(let key in object) {
+    if(callback(object[key])) {
+      return key;
+    }
+  }
+}
+
+
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
@@ -8,8 +18,11 @@ const assertEqual = function(actual, expected) {
 
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 2);
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma") // => "noma"
