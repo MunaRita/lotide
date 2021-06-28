c2d970ba73c7ea17be8a54ab6433862e3b49a121
vagrant <chiomaanierobi@gmail.com>
2021-06-28 17:56:16 +0000

    refactor of assertArraysEqual function to use module

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,18 @@
 // FUNCTION IMPLEMENTATION
-const eqArrays = function(arr1,arr2) {
-  if(arr1.length !== arr2.length) {
-    return false;
-  }
-  for(let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+
+////commnted codes below are before refoctoring to use modules
+// const eqArrays = function(arr1,arr2) {
+//   if(arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for(let i = 0; i < arr1.length; i++) {
+//     if(arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual,expected) {
   if (eqArrays(actual,expected)) {
@@ -28,8 +31,10 @@ const assertArraysEqual = function(actual,expected) {
 
 // };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+
+module.exports = assertArraysEqual;
