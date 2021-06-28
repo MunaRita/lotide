cd987bfc1fb6c5949a38a96542a6bc17054011bc
vagrant <chiomaanierobi@gmail.com>
2021-06-28 18:16:16 +0000

    refactor of middle function code to use module

--- middle.js
+++ middle.js
@@ -1,18 +1,21 @@
 // TEST ASSERTION FUNCTIONS
-const eqArrays = function(arr1, arr2) {
-  return Array.isArray(arr1) &&
-    Array.isArray(arr2) &&
-    arr1.length === arr2.length &&
-    arr1.every((val, index) => val === arr2[index]);
-}
+const assertArraysEqual = require("./assertArraysEqual");
 
-const assertArraysEqual = function(actual,expected) {
-  if (eqArrays(actual,expected)) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-  }
+//// commented out codes below are before refactoring to use module.
+// const eqArrays = function(arr1, arr2) {
+//   return Array.isArray(arr1) &&
+//     Array.isArray(arr2) &&
+//     arr1.length === arr2.length &&
+//     arr1.every((val, index) => val === arr2[index]);
+// }
+
+// const assertArraysEqual = function(actual,expected) {
+//   if (eqArrays(actual,expected)) {
+//     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+//   }
+//   }
 
   // ACTUAL FUNCTIONS
   const middle = function(array) {
@@ -27,7 +30,9 @@ const assertArraysEqual = function(actual,expected) {
   }
 
   
-  console.log(middle([1]));
-  console.log(middle([1, 2]));
-  console.log(middle([1, 2, 3, 4])); // => [2, 3]
-  console.log(middle([1, 2, 3, 4, 5, 6, 7])) // => 
+  // assertArraysEqual(middle([1]), []);
+  // assertArraysEqual(middle([1, 2]), []);
+  // assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+  // assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]) // => [4]
+
+  module.exports = middle;
