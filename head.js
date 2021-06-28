6db6e01cb0b3f1577772605e3817125ce2d001b5
vagrant <chiomaanierobi@gmail.com>
2021-06-28 17:02:38 +0000

    Implement Refactoring of head function using require

--- head.js
+++ head.js
@@ -1,15 +1,18 @@
 // FUNCTION IMPLEMENTATION
+const assertEqual = require('./assertEqual');
 const head = (actual) => {return (actual[0])};
 
-const assertEqual = function(actual, expected) {
+// const assertEqual = function(actual, expected) {
 
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+//   }
 
-};
+// };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
