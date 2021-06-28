d056f4a41d79a6f38ec567036a1a1cc510f83b18
vagrant <chiomaanierobi@gmail.com>
2021-06-28 23:35:10 +0000

    refactor of assertequal to use chai and mocha

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,7 +1,24 @@
-// TEST CODE
-const assertEqual = require('../assertEqual');
+// TEST CODE of assertEqual using chai
+const assert = require('chai').assert;
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 2);
+describe("#assertEqual", () => {
+  it("returns ✅✅✅ Assertion passed: Bootcamp === Bootcamp", () => {
+    assert.equal("Bootcamp", "Bootcamp");
+  });
+  
+  it("returns ✅✅✅ Assertion passed: 1 === 1", () => {
+    assert.equal(1, 1);
+   
+  });
+
+  it("returns ❌❌❌ Assertion Failed: Lighthouse Labs !== Bootcamp", () => {
+    assert.notEqual("Lighthouse Labs", "Bootcamp");
+  });
+
+  it("returns ❌❌❌ Assertion Failed: 1 !== 2", () => {
+    assert.notEqual(1, 2);
+  });
+
+});
+
+
