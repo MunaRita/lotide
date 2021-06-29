2efa27d66efdf0ec1400889707967520bc78529d
vagrant <chiomaanierobi@gmail.com>
2021-06-29 01:50:41 +0000

    linted and recfactored

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,13 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Light", "Labs"]);
+describe("#tail", () => {
+  it("returns [6, 7] for [5, 6, 7]", () => {
+    assert.deepEqual(tail([5, 6, 7]), [6, 7]);
+  });
+
+  it("returns [Lighthouse, Labs] for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+});
