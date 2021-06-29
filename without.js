9d8b7f8865944dc3cdec53c889cd8e99c0046aca
vagrant <chiomaanierobi@gmail.com>
2021-06-29 17:17:23 +0000

    Refactoring

--- without.js
+++ without.js
@@ -1,17 +1,14 @@
 const without = function(source, itemsToremove) {
-  let arr = []; // empty array to be populated with our new source after itemsToremove has been removed
+  let arr = [];
   for (let item of source) {
-      if(!itemsToremove.includes(item)) {
-        arr.push(item);
-      }
-      
+    if (!itemsToremove.includes(item)) {
+      arr.push(item);
     }
+      
+  }
   
   return arr;
-}
+};
 
-console.log(without([1, 2, 3], [1]))// => [2, 3]
 
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"]));
+module.exports = without;
