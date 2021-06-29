f6de0b2831ed6e23116a6eb4fd814ddc7245fa6a
vagrant <chiomaanierobi@gmail.com>
2021-06-29 01:59:27 +0000

    refactored and linted

--- middle.js
+++ middle.js
@@ -1,13 +1,12 @@
-// Implement function to find middle value of an array 
-  const middle = function(array) {
-    let middlevalue = Math.floor(array.length/2);
-    if(array.length <= 2) {
-      return [];
-    }
-    if(array.length % 2 === 0) {
-      return [array[middlevalue -1], array[middlevalue]];
-    }
-    return [array[middlevalue]];
+const middle = function(array) {
+  let middlevalue = Math.floor(array.length / 2);
+  if (array.length <= 2) {
+    return [];
   }
+  if (array.length % 2 === 0) {
+    return [array[middlevalue - 1], array[middlevalue]];
+  }
+  return [array[middlevalue]];
+};
 
-  module.exports = middle;
+module.exports = middle;
