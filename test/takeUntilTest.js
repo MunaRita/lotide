4d3107cd7715802fd39eaa77408d64f596837067
vagrant <chiomaanierobi@gmail.com>
2021-06-29 17:01:46 +0000

    refactoring

---
+++ test/takeUntilTest.js
@@ -0,0 +1,19 @@
+const takeUntil = require('../takeUntil');
+const assert = require('chai').assert;
+
+
+describe("#takeUntil", () => {
+  it("should return '[ 1, 2, 5, 7, 2 ]' for num", () => {
+    const num = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+    assert.deepEqual(takeUntil(num, x => x < 0), [ 1, 2, 5, 7, 2 ]);
+
+  });
+  it("should return [ 'I've', 'been', 'to', 'Hollywood' ] for str", () => {
+    const str = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+    assert.deepEqual(takeUntil(str, x => x === ','), [ "I've", 'been', 'to', 'Hollywood' ]);
+
+  });
+
+});
+
+
