6d0e395410ee71a23984255c905959042a760a00
vagrant <chiomaanierobi@gmail.com>
2021-06-29 02:31:56 +0000

    Refactored and linted

--- eqArrays.js
+++ eqArrays.js
@@ -1,32 +1,14 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = require('./assertEqual');
 const eqArrays = function(arr1,arr2) {
-  if(arr1.length !== arr2.length) {
+  if (arr1.length !== arr2.length) {
     return false;
   }
-  for(let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
-}
-
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-//   }
-
-// };
-// TEST CODE
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
-
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
-
+};
 
 module.exports = eqArrays;
 
