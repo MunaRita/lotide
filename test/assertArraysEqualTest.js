02d3fe8ee6e78738bb7aa0a66f711b97343a9ac8
vagrant <chiomaanierobi@gmail.com>
2021-06-29 02:27:24 +0000

    Refactored and linted

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,20 +1,21 @@
 const assert = require('chai').assert;
+const assertArraysEqual = require('../assertArraysEqual');
 
 describe("#assertArraysEqual", () => {
   it("returns ✅✅✅ Assertion passed: [1, 2, 3] === [1, 2, 3]", () => {
-    assert.deepEqual([1, 2, 3], [1, 2, 3]);  // => true
+    assert.deepEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));  // => true
   });
 
   it("returns ❌❌❌ Assertion Failed: [1, 2, 3] !== [3, 2, 1]", () => {
-    assert.notStrictEqual([1, 2, 3], [3, 2, 1]); // => false
+    assert.deepEqual(assertArraysEqual([1, 2, 3], [3, 2, 1])); // => false
   });
 
   it("returns ✅✅✅ Assertion passed: [\"1\", \"2\", \"3\"] === [\"1\", \"2\", \"3\"]", () => {
-    assert.deepEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+    assert.deepEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
   });
 
   it("returns ❌❌❌ Assertion Failed: [\"1\", \"2\", \"3\"] !== [\"1\", \"2\", 3]", () => {
-    assert.notStrictEqual(["1", "2", "3"], ["1", "2", 3]);  // => false
+    assert.deepEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));  // => false
   });
 
 });
