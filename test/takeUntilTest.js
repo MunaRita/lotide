0d75920cb5df4bfef5e243cfe48d1199e98e12dd
vagrant <chiomaanierobi@gmail.com>
2021-06-29 17:06:12 +0000

    refactoring

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -1,14 +1,13 @@
 const takeUntil = require('../takeUntil');
 const assert = require('chai').assert;
 
-
 describe("#takeUntil", () => {
-  it("should return '[ 1, 2, 5, 7, 2 ]' for num", () => {
+  it("should return '[ 1, 2, 5, 7, 2 ]' for [1, 2, 5, 7, 2, -1, 2, 4, 5]", () => {
     const num = [1, 2, 5, 7, 2, -1, 2, 4, 5];
     assert.deepEqual(takeUntil(num, x => x < 0), [ 1, 2, 5, 7, 2 ]);
 
   });
-  it("should return [ 'I've', 'been', 'to', 'Hollywood' ] for str", () => {
+  it("should return [ 'I've', 'been', 'to', 'Hollywood' ] for [\"I've\", \"been\", \"to\", \"Hollywood\", \",\", \"I've\", \"been\", \"to\", \"Redwood\"]", () => {
     const str = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
     assert.deepEqual(takeUntil(str, x => x === ','), [ "I've", 'been', 'to', 'Hollywood' ]);
 
