6120d98d0e72cf4f16fd57a49d224ee4100ce13e
vagrant <chiomaanierobi@gmail.com>
2021-06-29 02:18:23 +0000

    linted

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,4 @@
-// FUNCTION IMPLEMENTATION
 const eqArrays = require('./eqArrays');
-
 const assertArraysEqual = function(actual,expected) {
   if (eqArrays(actual,expected)) {
     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
