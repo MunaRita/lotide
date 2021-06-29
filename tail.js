7016da7c34be624419a4dc4136c2749a1a3f838e
vagrant <chiomaanierobi@gmail.com>
2021-06-29 01:44:16 +0000

    refactored and linted the code

--- tail.js
+++ tail.js
@@ -1,10 +1,9 @@
-const assertEqual = require('./assertEqual');
-
 const tail = function(actual) {
+  let arr = [];
   if (actual.length > 1) {
-    return actual.slice(actual.length - 1);
+    arr = actual.slice(1);
   }
-
+  return arr;
 };
 
 module.exports = tail;
