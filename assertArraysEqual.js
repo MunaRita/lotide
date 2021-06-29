111ed8077729498d2f73eee3b73f3bd82e630466
vagrant <chiomaanierobi@gmail.com>
2021-06-29 00:03:55 +0000

    Refactor(to use module) and linting of code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,4 @@
 // FUNCTION IMPLEMENTATION
-
-////commnted codes below are before refoctoring to use modules
-// const eqArrays = function(arr1,arr2) {
-//   if(arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for(let i = 0; i < arr1.length; i++) {
-//     if(arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
 const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual,expected) {
@@ -20,21 +7,7 @@ const assertArraysEqual = function(actual,expected) {
   } else {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
-  }
+};
 
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-//   }
-
-// };
-
-// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
 
 module.exports = assertArraysEqual;
